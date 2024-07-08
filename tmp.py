3df925e951a705261860d3db10516d6df649c28e
minwook <minkj1992@gmail.com>
2024-07-08 23:53:10 +0900

    feat: encoder mid

--- tmp.py
+++ tmp.py
@@ -1,51 +1,17 @@
-class Encoder(nn.Module):
-    """A encoder model with self attention mechanism."""
+import torch.nn.functional as F
 
-    def __init__(
-        self,
-        n_src_vocab,
-        d_word_vec,
-        n_layers,
-        n_head,
-        d_k,
-        d_v,
-        d_model,
-        d_inner,
-        pad_idx,
-        dropout=0.1,
-        n_position=200,
-    ):
 
-        super().__init__()
+class EncoderLayer(nn.Module):
+    """Compose with two layers"""
 
-        self.src_word_emb = nn.Embedding(n_src_vocab, d_word_vec, padding_idx=pad_idx)
-        self.position_enc = PositionalEncoding(d_word_vec, n_position=n_position)
-        self.dropout = nn.Dropout(p=dropout)
-        self.layer_stack = nn.ModuleList(
-            [
-                EncoderLayer(d_model, d_inner, n_head, d_k, d_v, dropout=dropout)
-                for _ in range(n_layers)
-            ]
+    def __init__(self, d_model, num_head, d_k, d_v, dropout=0.1):
+        super(EncoderLayer, self).__init__()
+        self.slf_attn = MultiHeadAttention(num_head, d_model, d_k, d_v, dropout=dropout)
+        self.pos_ffn = PositionwiseFeedForward(d_model, dropout=dropout)
+
+    def forward(self, enc_input, attn_mask=None):
+        enc_output, enc_slf_attn = self.slf_attn(
+            enc_input, enc_input, enc_input, mask=attn_mask
         )
-        self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
-        self.scale_emb = scale_emb
-        self.d_model = d_model
-
-    def forward(self, src_seq, src_mask, return_attns=False):
-
-        enc_slf_attn_list = []
-
-        # -- Forward
-        enc_output = self.src_word_emb(src_seq)
-        if self.scale_emb:
-            enc_output *= self.d_model**0.5
-        enc_output = self.dropout(self.position_enc(enc_output))
-        enc_output = self.layer_norm(enc_output)
-
-        for enc_layer in self.layer_stack:
-            enc_output, enc_slf_attn = enc_layer(enc_output, slf_attn_mask=src_mask)
-            enc_slf_attn_list += [enc_slf_attn] if return_attns else []
-
-        if return_attns:
-            return enc_output, enc_slf_attn_list
-        return (enc_output,)
+        enc_output = self.pos_ffn(enc_output)
+        return enc_output, enc_slf_attn
